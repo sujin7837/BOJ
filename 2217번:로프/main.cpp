3e488845be62f541297abc65533a28f464f1f9d9
sujin7837 <sujin7837@naver.com>
2021-02-14 14:43:20 +0900

    Initial Commit

---
+++ 2217번:로프/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  2217번:로프
+//
+//  Created by 김수진 on 2021/02/14.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
