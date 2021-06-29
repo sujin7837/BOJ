c409cfdee0b7364cf1e5a5b665229222b715db56
sujin7837 <sujin7837@gmail.com>
2021-06-29 15:27:02 +0900

    백준 2798번

--- 2798번 블랙잭/2798번 블랙잭/main.cpp
+++ 2798번 블랙잭/2798번 블랙잭/main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 int main() {
     int n, m;
     int sum=0;
+    int result=0;
     int cards[100];
     cin>>n>>m;
     
@@ -22,9 +23,11 @@ int main() {
         for(int j=i+1;j<n-1;j++) {
             for(int k=j+1;k<n;k++) {
                 sum=cards[i]+cards[j]+cards[k];
+                if(sum<=m && sum>result) result=sum;
             }
         }
     }
+    cout<<result<<endl;
     
     return 0;
 }
