f58a32a65a7c3b7b7fa1579479121baa4bafc426
sujin7837 <sujin7837@gmail.com>
2021-09-25 20:53:53 +0900

    1246번 온라인 판매

---
+++ 1005번 ACM Craft/1005번 ACM Craft/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  1005번 ACM Craft
+//
+//  Created by 김수진 on 2021/09/11.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t, n, k;
+    int d[4];
+    
+    
+    cin>>t;
+    for(int i=0;i<t;i++) {
+        cin>>n>>k;
+        for(int j=0;j<n;j++) cin>>d[j];
+        
+    }
+    
+    return 0;
+}
