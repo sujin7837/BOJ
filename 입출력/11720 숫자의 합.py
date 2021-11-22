fd7d09df5a53703af297a59414e68c45430732be
sujin7837 <sujin7837@gmail.com>
2021-11-23 01:47:56 +0900

    입출력

---
+++ 입출력/11720 숫자의 합.py
@@ -0,0 +1,7 @@
+n = int(input())
+num = list(input())
+
+sum = 0
+for x in num:
+    sum += int(x)
+print(sum)
---
+++ 입출력/11721 열 개씩 끊어 출력하기.py
@@ -0,0 +1,7 @@
+s = input()
+
+a = 0
+while a < len(s):
+    print(s[a:a+10])
+    a += 10
+
