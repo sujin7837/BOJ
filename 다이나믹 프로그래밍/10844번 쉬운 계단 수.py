490ef3da4ea970b4efcd7a48e43d249d48f675b9
sujin7837 <sujin7837@naver.com>
2021-11-30 13:23:01 +0900

    오르막 수

--- 다이나믹 프로그래밍/10844번 쉬운 계단 수.py
+++ 다이나믹 프로그래밍/10844번 쉬운 계단 수.py
@@ -1,15 +1,3 @@
-# n = int(input())
-# dp = [[0] * 101 for _ in range(11)]
-
-# for i in range(1, 10):
-#     dp[1][i] = 1
-
-# for i in range(2, n+1):
-#     for j in range(1, 11):
-#         dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
-
-# print(sum(dp[n]))
-
 n = int(input())
 dp = [[0] * 12 for _ in range(101)]
 
