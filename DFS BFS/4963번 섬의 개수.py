12512ab07aa845c5c5aacd4c31915a6b854e2a46
sujin7837 <sujin7837@naver.com>
2021-12-24 00:01:29 +0900

    섬의 개수

--- DFS BFS/4963번 섬의 개수.py
+++ DFS BFS/4963번 섬의 개수.py
@@ -1,11 +1,31 @@
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+def dfs(x, y):
+    visit[x][y] = True
+
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+
+        if nx >= 0 and nx < w and ny >= 0 and ny < h:
+            dfs(nx, ny)
+
+
+
 while True:
     w, h = map(int, input().split())
     if w == 0 and h == 0:
         break
     else:
         li = [[] for _ in range(h)]
+        visit = [[False] * (w * h)]
+        cnt = 0
+
         for i in range(h):
             li[i] = list(map(int, input().split()))
-        print(li)
-        # for i in range(w):
-        #     for j in range(h):
+        for i in range(w):
+            for j in range(h):
+                if not visit[i][j]:
+                    cnt += 1
+                    dfs(i, j)
