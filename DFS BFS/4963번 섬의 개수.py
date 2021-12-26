8055afc6e8fc052f8f219bd91f1f7368209767bc
sujin7837 <sujin7837@naver.com>
2021-12-26 17:15:46 +0900

    섬의 개수

--- DFS BFS/4963번 섬의 개수.py
+++ DFS BFS/4963번 섬의 개수.py
@@ -1,36 +1,36 @@
 import sys
 sys.setrecursionlimit(10 ** 6)
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
+dx = [-1, -1, -1, 1, 1, 1, 0, 0]
+dy = [-1, 0, 1, -1, 0, 1, -1, 1]
 
 def dfs(x, y):
     visit[x][y] = True
 
-    for i in range(4):
+    for i in range(7):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if nx >= 0 and nx < w and ny >= 0 and ny < h:
-            dfs(nx, ny)
+        if nx >= 0 and nx < h and ny >= 0 and ny < w:
+            if not visit[nx][ny] and li[nx][ny] == 1:
+                dfs(nx, ny)
 
 
 
 while True:
     w, h = map(int, input().split())
-    print(w, h)
     if w == 0 and h == 0:
-        print('haha')
         break
     else:
         li = [[] for _ in range(h)]
-        visit = [[False] * (w * h) for _ in range(w * h) ] 
+        visit = [[False] * w for _ in range(h)] 
         cnt = 0
 
         for i in range(h):
             li[i] = list(map(int, input().split()))
-        for i in range(w):
-            for j in range(h):
-                if not visit[i][j]:
+        for i in range(h):
+            for j in range(w):
+                if not visit[i][j] and li[i][j] == 1:
                     cnt += 1
                     dfs(i, j)
+        print(cnt)
