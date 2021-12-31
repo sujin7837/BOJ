a2a1cf7da48d87f623b82b3124f9cdbe4185e18b
sujin7837 <sujin7837@naver.com>
2021-12-31 09:25:14 +0900

    트리 순회

--- DFS BFS/1991번 트리 순회.py
+++ DFS BFS/1991번 트리 순회.py
@@ -1,10 +1,36 @@
-from collections import deque
-
 def preorder(x):
-    
+    if x != '.':
+        for i in range(n):
+            if li[i][0] == x:
+                num = i
+        print(x, end='')
+        preorder(li[num][1])
+        preorder(li[num][2])
+
+def inorder(x):
+    if x != '.':
+        for i in range(n):
+            if li[i][0] == x:
+                num = i
+        inorder(li[num][1])
+        print(x, end='')
+        inorder(li[num][2])
+
+def postorder(x):
+    if x != '.':
+        for i in range(n):
+            if li[i][0] == x:
+                num = i
+        postorder(li[num][1])
+        postorder(li[num][2])
+        print(x, end='')
+
 
 n = int(input())
 li = [list(input().split()) for i in range(n)]
 
 preorder('A')
-
+print('')
+inorder('A')
+print('')
+postorder('A')
