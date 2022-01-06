dfc4ab8d014e4586fd8646ef1a96e1a1979cb20e
sujin7837 <sujin7837@naver.com>
2022-01-06 23:27:30 +0900

    트리의 지름

--- DFS BFS/1167번 트리의 지름.py
+++ DFS BFS/1167번 트리의 지름.py
@@ -1,11 +1,13 @@
 from collections import deque
+import sys
 
-v = int(input())
+read = sys.stdin.readline
+v = int(read())
 graph = [[] for _ in range(v+1)]
 
 
 for i in range(v):
-    li = list(map(int, input().split()))
+    li = list(map(int, read().split()))
     for j in range(1, len(li)-2, 2):
         graph[li[0]].append((li[j], li[j+1]))
 
