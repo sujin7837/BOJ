a7943e7fbd260b14fb5c35f4caaa6ad0a6c5b0c7
sujin7837 <sujin7837@naver.com>
2022-01-12 23:49:14 +0900

    민호와 강호

---
+++ 이진 탐색/2110번 공유기 설치.py
@@ -0,0 +1,23 @@
+n, c = map(int, input().split())
+li = []
+
+for i in range(n):
+    li.append(int(input()))
+li.sort()
+
+start, end = 1, li[-1] - li[0]
+while start <= end:
+    cnt = 1
+    mid = (start + end) // 2
+    now = li[0]
+    for i in range(1, n):
+        if li[i] >= mid + now:
+            cnt += 1
+            now = li[i]
+    if cnt >= c:
+        start = mid + 1
+        result = mid
+    else:
+        end = mid - 1
+
+print(result)
