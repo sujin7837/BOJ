7c6d6b19627efc7fcb92329439086a5a026b489a
sujin7837 <sujin7837@naver.com>
2022-01-18 23:36:07 +0900

    별 찍기

--- 분할정복/2447번 별 찍기 - 10.py
+++ 분할정복/2447번 별 찍기 - 10.py
@@ -1,11 +1,13 @@
 n = int(input())
-li = [['***'], ['* *'], ['***']]
-result = []
+li = [['*','*','*'], ['*',' ','*'], ['*','*','*']]
+result = [[' '] * n for _ in range(n)]
 
 def star(x, y, s):
     global result
     if s == 3:
-        result.append(li)
+        for i in range(3):
+            for j in range(3):
+                result[x+i][y+j]=li[i][j]
     else:
         s //= 3
         for i in range(3):
@@ -14,15 +16,9 @@ def star(x, y, s):
                     continue
                 star(x+i*s, y+j*s, s)
 star(0, 0, n)
-for i in range(len(result)):
-    if i % 3 == 0:
-        print(result[i][0][0])
-        print(result[i][1][0])
-        print(result[i][2][0])
 
-for i in range(n//3):        
-    print(result[i][0])
 
-# for i in range(n):
-#     for j in range(n):
-#         print(result)
+for i in range(n):
+    for j in range(n):
+        print(result[i][j], end='')
+    print()
